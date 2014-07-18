4e7fcc0f38207d27788954dc6d8af9154321fcc8
mike pupo <mike.pupo@icloud.com>
2014-07-18 01:10:32 +0000

    menu toggles responsively

--- classwork/04-responsive-menu/js/scripts.js
+++ classwork/04-responsive-menu/js/scripts.js
@@ -1,16 +1,20 @@
+
 $(document).ready(function() {
   var ww = document.body.clientWidth;
+  $(".toggleMenu").click(function(e) {
+    e.preventDefault();
+    $(".nav").toggle();
+  });
+  
   $(".nav li a").each(function() {
     if ($(this).next().length > 0) {
       $(this).addClass("parent");
     } 
   });
-  
-  
-  
-  
-  if (ww < 800) {
+ 
+    if (ww < 800) {
     $(".toggleMenu").css("display", "inline-block");
+    $(".nav").hide();
     $(".nav li a").click(function() {
       $(this).parent("li").toggleClass("hover");
     });
@@ -23,5 +27,5 @@ $(document).ready(function() {
     });
   }
   
-  
-});
+}); // end of document.ready function
+
