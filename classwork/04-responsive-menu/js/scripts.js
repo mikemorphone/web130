f02fa560a1d25df1bdc8272046c8c745f13678bb
mike pupo <mike.pupo@icloud.com>
2014-07-18 00:52:59 +0000

    made menu responsive now

---
+++ classwork/04-responsive-menu/js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var ww = document.body.clientWidth;
+  $(".nav li a").each(function() {
+    if ($(this).next().length > 0) {
+      $(this).addClass("parent");
+    } 
+  });
+  
+  
+  
+  
+  if (ww < 800) {
+    $(".toggleMenu").css("display", "inline-block");
+    $(".nav li a").click(function() {
+      $(this).parent("li").toggleClass("hover");
+    });
+  } else {
+    $("toggleMenu").css("display", "none");
+    $(".nav li").hover(function() {
+      $(this).addClass("hover");
+    }, function() {
+      $(this).removeClass("hover");
+    });
+  }
+  
+  
+});
